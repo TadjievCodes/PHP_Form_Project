59123ae727eb6f745bf7ba705159e530ed1f1229
TadjievCodes <muhammad2008.08@mail.ru>
2020-07-15 21:04:30 -0400

    test2

---
+++ test2.php
@@ -0,0 +1,102 @@
+/ Start of the Test 2
+
+<pre> 
+
+<?php
+
+
+	
+//$a = 5;
+//$b = 6;
+
+//echo  $a--;
+
+
+//echo $a * $b;
+
+echo '<br/>';
+echo '<br>';
+echo '<br>';
+
+
+$x=5;
+
+if( $x = 7 ){ 
+    echo 'yes';
+}else{
+    echo 'no';
+}
+
+
+echo '<br>';
+echo '<br>';
+
+
+
+
+
+
+$start = 'Today is ';
+$start .= date("D M n, o");
+echo $start . ' and this outputs the current date';
+
+
+echo '<br>';
+echo '<br>';
+
+
+/*
+$a = 5;
+$b = 3;
+echo 'StepA: '. ++$a . "<br>";
+echo 'StepB: ' . $b *= $a;
+echo '<br>StepC: ' . $b++;
+*/
+
+
+/*
+$a = 2;
+$b = 5;
+
+if($a >= 2 && $b <= 4 || $b == 5){
+  echo 'yes';
+}else{
+  echo 'no';
+};
+
+*/
+
+
+for ($x = 1; $x <= 6; ++$x) { // $x will equal 1,2,3,4,5
+  echo "The value of x is $x <br>";
+}
+
+
+
+echo '<br>';
+echo '<br>';
+
+
+ echo $abror = "hey hows $start it going?"
+
+
+//for each($array as $key = $val);
+/*
+$array = [1,2,3];
+
+
+$val = 'Do this';
+
+echo $array[2] = $val;
+*/
+/*
+$myArray [ 'p1' => 'Mercury' , 'p12' => 'Venus' , 'p3' => 'Earth' ];
+
+ $myArray['Venus'];
+*/
+
+
+?>
+
+
+</pre>
